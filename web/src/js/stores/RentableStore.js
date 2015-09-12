bf692063829c23f0f5dca7606b6b86638a45441c
Mattis Kancans Envall <mattiskan@gmail.com>
2015-09-12 18:47:54 +0200

    rentables will now fetch data from backend

--- web/src/js/stores/RentableStore.js
+++ web/src/js/stores/RentableStore.js
@@ -6,51 +6,20 @@ import assign from 'object-assign';
 var fetch = require('fetch').fetchUrl;
 
 // data storage
-let _data = [];
+let _data = {rentables: []};
 
-// add private functions to modify data
-function addItem(title, completed=false) {
-  _data.push({title, completed});
-}
 
 // Facebook style store creation.
 const RentableStore = assign({}, BaseStore, {
   // public methods used by Controller-View to operate on data
   getAll() {
 
-    var loaded_rentables = fetch(
-      'http://localhost:3000/rentables.json',
-      function(error, meta, body) {
-        console.log(body);
-      }
-    );
-
-
-
+    fetch('http://localhost:3000/rentables.json', function(error, meta, body) {
+      _data['rentables'] = JSON.parse(body.toString());
+      RentableStore.emitChange();
+    });
     
-    return {
-      rentables: [
-        {
-          "id":1,
-          "description":"cool thing",
-          "price":"9001.0",
-          "owner_id":2,
-          "url": "rentable/1",
-          "created_at":"2015-09-12T12:34:07.674Z",
-          "updated_at":"2015-09-12T12:34:07.674Z"
-        },
-        {
-          "id":2,
-          "description":"cool thing",
-          "price":"9001.0",
-          "owner_id":2,
-          "url": "rentable/2",
-          "created_at":"2015-09-12T12:34:07.674Z",
-          "updated_at":"2015-09-12T12:34:07.674Z"
-        },
-      ]
-    };
-
+    return _data;
   },
 
   // register store with dispatcher, allowing actions to flow through
