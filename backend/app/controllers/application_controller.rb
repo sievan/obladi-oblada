166143fe674a92c628e04609cf3e836b81455d1f
Christian Avemark <chav02@kth.se>
2015-09-12 20:19:04 +0200

    skip automatic csrf

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
