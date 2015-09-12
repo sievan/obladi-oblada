013ba26931f5f2539f14399e22ab9ff34038b728
Christian Avemark <chav02@kth.se>
2015-09-12 22:39:34 +0200

    images

--- backend/app/controllers/rentables_controller.rb
+++ backend/app/controllers/rentables_controller.rb
@@ -63,12 +63,13 @@ class RentablesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_rentable
-      @rentable = Rentable.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def rentable_params
-      params.require(:rentable).permit(:description, :price, :owner_id, :title)
-    end
+  def set_rentable
+    @rentable = Rentable.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def rentable_params
+    params.require(:rentable).permit(:description, :price, :owner_id, :title, :image)
+  end
 end
