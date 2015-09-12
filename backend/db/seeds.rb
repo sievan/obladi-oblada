47187b4dff6e638a114fa9b41dbadf33f88a3922
Christian Avemark <chav02@kth.se>
2015-09-12 14:59:11 +0200

    add seeds!

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+names =  ["Jocke", "Osquar", "Lasse Kongo", "Embraquel D Tuta"]
+
+names.each do |n|
+  User.create name: names[n]
+end
+
+User.each do |u|
+  Owner.create user: u
+  (1..5).to_a.sample.times do |n|
+    Rentable.create owner: u.owner, description: "#{u.name}s coola grej #{n}"
+  end
+end
+
+User.each do |u|
+  Rental.create user: u
+  (1..5).to_a.sample.times do |n|
+
+  end
+end
