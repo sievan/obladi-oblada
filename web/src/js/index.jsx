eb3b3d0d8978b3b02d979c8151abfa883bbd8482
Simon Strom <simon.strom@gmail.com>
2015-09-12 18:11:12 +0200

    add navigation bar and extract pages

--- web/src/js/index.jsx
+++ web/src/js/index.jsx
@@ -12,6 +12,9 @@ import NavigationBar from './components/NavigationBar.jsx';
 
 import SignIn from  './components/authentication/SingIn.jsx';
 
+import Home from './components/pages/Home.jsx';
+import About from './components/pages/About.jsx';
+import NavigationBar from './components/NavigationBar.jsx';
 
 var App = React.createClass({
   handleAuthentication() {
@@ -35,6 +38,17 @@ var RentList = React.createClass({
   render() {
     return (
       <RentablesList />
+
+        <div className="container">
+          {/* change the <a>s to <Links>s */}
+
+          {/*
+            next we replace `<Child>` with `this.props.children`
+            the router will figure out the children for us
+          */}
+          {this.props.children}
+        </div>
+      </div>
     )
   }
 })
