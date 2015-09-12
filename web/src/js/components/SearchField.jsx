c7dca6627f0ae2d5c2e23b50f1915fe44651cdba
Mattis Kancans Envall <mattiskan@gmail.com>
2015-09-12 23:58:10 +0200

    added search field

---
+++ web/src/js/components/SearchField.jsx
@@ -0,0 +1,11 @@
+import React, {PropTypes} from 'react';
+import {Input} from 'react-bootstrap';
+
+export default React.createClass({
+
+  render() {
+    return (
+      <Input type="text" placeholder="Enter text to search..." />
+    );
+  }
+});
