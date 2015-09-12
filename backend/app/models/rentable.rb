a0c0d7bef6b5699e6d00a65370c50fbebc7ffe9d
Christian Avemark <chav02@kth.se>
2015-09-12 22:39:34 +0200

    images

--- backend/app/models/rentable.rb
+++ backend/app/models/rentable.rb
@@ -5,4 +5,15 @@ class Rentable < ActiveRecord::Base
   def owner_name
     owner.user.name
   end
+
+  def image=(image)
+    uploaded_io = image
+    File.open(
+      Rails.root.join('public', 'uploads', uploaded_io.original_filename),
+      'wb'
+    ) do |file|
+      file.write(uploaded_io.read)
+    end
+    self[:image] = image.original_filename
+  end
 end
