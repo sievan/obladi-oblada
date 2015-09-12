0770f3251d5895647024d6d155703473d7caeb14
Jacob Sievers <jacosiev@gmail.com>
2015-09-12 20:37:45 +0200

    Change key name from name to title for rentables

--- web/src/js/components/CreateListingForm.jsx
+++ web/src/js/components/CreateListingForm.jsx
@@ -4,10 +4,10 @@ import Input from 'react-bootstrap/lib/Input';
 export default React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
-    var name = this.refs.name.getValue()
+    var title = this.refs.title.getValue()
     var description = this.refs.description.getValue()
     var price = this.refs.price.getValue()
-    if (!description || !name || !price) {
+    if (!description || !title || !price) {
       return;
     }
 
@@ -18,13 +18,13 @@ export default React.createClass({
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        name: name,
+        title: title,
         description: description,
         price: price
       })
     })
 
-    this.refs.name.getInputDOMNode().value = '';
+    this.refs.title.getInputDOMNode().value = '';
     this.refs.description.getInputDOMNode().value = '';
     this.refs.price.getInputDOMNode().value = '';
     return;
@@ -32,7 +32,7 @@ export default React.createClass({
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
-        <Input type="text" label="Name" placeholder="Name of item" ref="name" />
+        <Input type="text" label="Name" placeholder="Name of item" ref="title" />
         <Input type="text" label="Description" placeholder="Short description" ref="description" />
         <Input type="text" label="Price" placeholder="Price" ref="price" />
         <Input type="submit" value="Post" />
