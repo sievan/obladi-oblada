341c8aefc13a26041eef6b12366e4270810bcfb3
Christian Avemark <chav02@kth.se>
2015-09-12 14:32:28 +0200

    changes.

--- backend/app/models/owner.rb
+++ backend/app/models/owner.rb
@@ -1,3 +1,4 @@
 class Owner < ActiveRecord::Base
   belongs_to :user
+  delegates :user, :name
 end
--- backend/app/models/rentable.rb
+++ backend/app/models/rentable.rb
@@ -1,3 +1,7 @@
 class Rentable < ActiveRecord::Base
   belongs_to :owner
+
+  def owner_name
+    owner.user.name
+  end
 end
