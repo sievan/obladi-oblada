1d66d57a391008ad7a1e48644e2b360b4d16e53e
Mattis Kancans Envall <mattiskan@gmail.com>
2015-09-12 23:58:10 +0200

    added search field

--- web/src/js/components/RentablesList.jsx
+++ web/src/js/components/RentablesList.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import RentableStore from '../stores/RentableStore';
 import Rentable from './Rentable.jsx';
+import SearchField from './SearchField.jsx';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import Alert from 'react-bootstrap/lib/Alert';
 import {Button} from 'react-bootstrap';
@@ -21,6 +22,8 @@ export default React.createClass({
 
     return (
       <div>
+        <SearchField />
+        
         <ListGroup>
         {rentables.map(rentable =>
           <Rentable key={rentable.id} rentable={rentable}/>
