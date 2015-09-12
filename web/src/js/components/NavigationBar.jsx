4643183a7804800fbc30c18ee2ab3ea02fa9e61f
Simon Strom <simon.strom@gmail.com>
2015-09-12 19:11:40 +0200

    add signin button to navbar

--- web/src/js/components/NavigationBar.jsx
+++ web/src/js/components/NavigationBar.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import { Link } from 'react-router'
 
+import SignIn from './authentication/SignIn.jsx';
+
 var NavigationBar = React.createClass({
   render() {
     return (
-      <Navbar brand="React-Bootstrap">
-        <Nav>
-          <NavItem><Link to="/home">Home</Link></NavItem>
-          <NavItem><Link to="/about">About</Link></NavItem>
-          <NavItem><Link to="/rentlist">I wanna rent someting!</Link></NavItem>
-          <NavItem><Link to="/pay">Do Payment!</Link></NavItem>
-        </Nav>
-      </Navbar>
+      <div>
+        <Navbar brand="React-Bootstrap">
+          <Nav>
+            <NavItem><Link to="/home">Home</Link></NavItem>
+            <NavItem><Link to="/about">About</Link></NavItem>
+            <NavItem><Link to="/rentlist">I wanna rent someting!</Link></NavItem>
+
+            <NavItem><Link to="/pay">Do Payment!</Link></NavItem>
+            <NavItem><SignIn /></NavItem>
+          </Nav>
+        </Navbar>
+      </div>
     )
   }
 });
