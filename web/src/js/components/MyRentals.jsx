e4a19b1ac0e41e6ca4b77cad9b3142e6ee6232e3
Jacob Sievers <jacosiev@gmail.com>
2015-09-13 02:18:56 +0200

    Add boilerplate to get own rentables

---
+++ web/src/js/components/MyRentals.jsx
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+import RentableStore from '../stores/RentableStore';
+import Rentable from './Rentable.jsx';
+import ListGroup from 'react-bootstrap/lib/ListGroup';
+import Alert from 'react-bootstrap/lib/Alert';
+import {Button} from 'react-bootstrap';
+
+export default React.createClass({
+
+  getInitialState() {
+    RentableStore.addChangeListener(this._onUpdate);
+    return RentableStore.getAll();
+  },
+
+  _onUpdate() {
+    this.forceUpdate();
+  },
+
+  render() {
+    let {rentables} = this.state;
+
+    return (
+      <div>
+        <ListGroup>
+        {rentables.map(rentable =>
+          <Rentable key={rentable.id} rentable={rentable}/>
+        )}
+        </ListGroup>
+      </div>
+    );
+  }
+});
