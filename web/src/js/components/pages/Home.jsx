d9b1cd860117f6e7fd1d182ff3dc785d89d088d0
Mattis Kancans Envall <mattiskan@gmail.com>
2015-09-13 10:46:11 +0200

    bg test

--- web/src/js/components/pages/Home.jsx
+++ web/src/js/components/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 var Home = React.createClass({
+  componentWillMount: function(){
+
+    document.body.style.backgroundImage = 'url("http://blog.visitnapavalley.com/wp-content/uploads/2014/07/napa-valley-bicycles-iStock_000015544672Large.jpg")';
+
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'left 20%';
+  },
+  componentWillUnmount: function(){
+    document.body.style.background = null;
+  },
+
   render() {
     return (
-      <div>
+      <div className="fulhack" >
         Welcome to obladi!
       </div>
-    )
+    );
   }
 });
 
